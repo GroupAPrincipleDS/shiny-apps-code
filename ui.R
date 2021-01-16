8912553b2956d20c6d8c2e1744b395ea08a6da82
weijie-95 <73516399+weijie-95@users.noreply.github.com>
2021-01-16 21:15:58 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -38,25 +38,25 @@ root_page <-     # Sidebar layout with a product search input and table output
 #Add in the documenation into following page 
 documentation_page <- div(
     br(),
-    p(strong("Introduction"), style = "font-size:500px;"),
+    p(strong("Introduction"), style = "font-size:50px;"),
     p("In most of the common rating system only showcase the average rating scale from 1 to 5. However, this type of rating showcase is too general and if buyer want to know the true comments or reviews of the interested product, they will have to browser through the comment section one by one.
       Therefore, the purpose of this apps is to help buyers to quickly browse through the review summaries and gain more detailed understanding on the product via word cloud. Word Cloud is a visualization tool where an imaged composed of words used in a particular text or subject, 
       in which the size of each word indicates its frequency or importance. Two word clouds are being generated to analysis the positive reviews and negative reviews.",style="text-align:justify;color:black;background-color:lavender;padding:15px;border-radius:10px"),
     br(),
     br(),
-    p(strong("Apps Guideline"), style = "font-size:500px;"),
+    p(strong("Apps Guideline"), style = "font-size:50px;"),
       p("1. User can search the interested appliances in the search column. User can choose to narrow the search by selecting the brand of ideal appliances.",
         br(),"2. A table with the selected type of appliances will be listed based on the review counts.",
         br(),"3. User can further select the product interested to view the word cloud. Table of the review's summary also will be shown.",style="text-align:justify;color:black;background-color:lavender;padding:15px;border-radius:10px"),
       br(),
       br(),
-      p(strong("Dataset"), style = "font-size:500px;"),
+      p(strong("Dataset"), style = "font-size:50px;"),
         p("The review dataset used in building this apps is the Appliances's data retrieved from Amazon (2018) where it contained total of 602,777 reviews and summaries. The dataset is introduced into the SQL server for data pre-processing purpose.",style="text-align:justify;color:black;background-color:lavender;padding:15px;border-radius:10px"),
         p("For more information regardign the dataset please check the",em("Amazon Review Data (2018)'s"),"page by clicking",
           a(href="http://deepyeti.ucsd.edu/jianmo/amazon/index.html", "Here",target="_blank"),style="text-align:center;color:black",width=2),
     br(),
     br(),
-    p(strong("Future Works"), style = "font-size:500px;font-family:times"),
+    p(strong("Future Works"), style = "font-size:50px;font-family:times"),
     p("Due to the limitation of the local drive's processing power, this review system only able to conduct the sentiment analysis of a single catergory which is the 'Appliances' in the Amazon product review data.
       Future works can be carry out by including all categories of product in the Amazon review data (2018).",style="text-align:justify;color:black;background-color:lavender;padding:15px;border-radius:10px")
       )
